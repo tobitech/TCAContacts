60983bdd874fd4d301f90a8ec248a61d4baa5caa
Tobi Omotayo <tobitech@ymail.com>
2023-07-12 15:47:29 +0100

    integrate the two reducers to establish communication

--- TCAContacts/ContactsFeature.swift
+++ TCAContacts/ContactsFeature.swift
@@ -15,18 +15,41 @@ struct Contact: Equatable, Identifiable {
 
 struct ContactsFeature: ReducerProtocol {
 	struct State: Equatable {
+		@PresentationState var addContact: AddContactFeature.State?
 		var contacts: IdentifiedArrayOf<Contact> = []
 	}
 	enum Action: Equatable {
 		case addButtonTapped
+		case addContact(PresentationAction<AddContactFeature.Action>)
 	}
 	var body: some ReducerProtocolOf<Self> {
 		Reduce { state, action in
 			switch action {
 			case .addButtonTapped:
-				// TODO: Handle action
+				state.addContact = AddContactFeature.State(
+					contact: Contact(
+						id: UUID(),
+						name: ""
+					)
+				)
+				return .none
+				
+			case .addContact(.presented(.cancelButtonTapped)):
+				state.addContact = nil
+				return .none
+				
+			case .addContact(.presented(.saveButtonTapped)):
+				guard let contact = state.addContact?.contact else { return .none }
+				state.contacts.append(contact)
+				state.addContact = nil
+				return .none
+
+			case .addContact:
 				return .none
 			}
+		}
+		.ifLet(\.$addContact, action: /Action.addContact) {
+			AddContactFeature()
 		}
 	}
 }
