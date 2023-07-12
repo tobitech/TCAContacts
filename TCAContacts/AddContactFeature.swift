8afff7654320a8406ee509f44e85daae7dbfeee0
Tobi Omotayo <tobitech@ymail.com>
2023-07-12 15:28:41 +0100

    setup view and reducer for add contact form

---
+++ TCAContacts/AddContactFeature.swift
@@ -0,0 +1,66 @@
+import ComposableArchitecture
+import SwiftUI
+
+struct AddContactFeature: ReducerProtocol {
+	struct State: Equatable {
+		var contact: Contact
+	}
+	enum Action: Equatable {
+		case cancelButtonTapped
+		case saveButtonTapped
+		case setName(String)
+	}
+	func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+		switch action {
+		case .cancelButtonTapped:
+			return .none
+
+		case .saveButtonTapped:
+			return .none
+
+		case let .setName(name):
+			state.contact.name = name
+			return .none
+		}
+	}
+}
+
+struct AddContactView: View {
+	let store: StoreOf<AddContactFeature>
+
+	var body: some View {
+		WithViewStore(self.store, observe: { $0 }) { viewStore in
+			Form {
+				TextField("Name", text: viewStore.binding(get: \.contact.name, send: { .setName($0) }))
+				Button("Save") {
+					viewStore.send(.saveButtonTapped)
+				}
+			}
+			.toolbar {
+				ToolbarItem {
+					Button("Cancel") {
+						viewStore.send(.cancelButtonTapped)
+					}
+				}
+			}
+		}
+	}
+}
+
+struct AddContactPreviews: PreviewProvider {
+	static var previews: some View {
+		NavigationStack {
+			AddContactView(
+				store: Store(
+					initialState: AddContactFeature.State(
+						contact: Contact(
+							id: UUID(),
+							name: "Blob"
+						)
+					),
+					reducer: AddContactFeature()
+				)
+			)
+		}
+	}
+}
