8ccbf7d7f88222503db10acda35a3c4931652f5f
Tobi Omotayo <tobitech@ymail.com>
2023-07-12 15:19:10 +0100

    Initial Commit

---
+++ TCAContacts/TCAContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAContactsApp.swift
+//  TCAContacts
+//
+//  Created by Oluwatobi Omotayo on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
